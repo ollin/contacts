2d2e86f0cb1003d6daae0b7903548d41440e1ccc
Oliver Nautsch <oliver@brixel.io>
2024-04-02 17:46:38 +0200

    integrate npx tailwind into gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,20 @@
+import com.github.gradle.node.npm.task.NpxTask
+import java.util.Properties
+
 plugins {
+    idea
+    base
     kotlin("jvm") version libs.versions.kotlin
     id("io.ktor.plugin") version libs.versions.ktor
+    alias(libs.plugins.nodePlugin)
 }
 
-group = "com.nautsch"
-version = "0.0.1"
+// see gradle.properties
+val versionMajor: String by project
+val versionMinor: String by project
+
+group = "com.nautsch.contacts"
+version = createProjectVersion()
 
 application {
     mainClass.set("com.nautsch.ApplicationKt")
@@ -35,4 +45,62 @@ dependencies {
 
     testImplementation(libs.ktor.server.tests.jvm)
     testImplementation(libs.bundles.kotest)
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes(
+            mapOf(
+                "Implementation-Title"  to project.name,
+                "Implementation-Version" to project.version
+            )
+        )
+    }
+}
+
+
+tasks.register("generateTailwindCSS", NpxTask::class) {
+    command.set("tailwindcss")
+    args.set(listOf(
+        "-m",
+        "-i", "./src/main/frontend/input.css",
+        "-o", "./build/resources/main/public/css/output.css",
+    ))
+    inputs.files(
+        file("./src/main/frontend/input.css"),
+        file("./tailwind.config.js"),
+    )
+    outputs.files(
+        file("./build/resources/main/public/css/output.css")
+    )
+}
+
+tasks.named("processResources") {
+    finalizedBy("generateTailwindCSS")
+}
+
+tasks.named("jar") {
+    dependsOn("generateTailwindCSS")
+}
+
+tasks.named("shadowJar") {
+    dependsOn("generateTailwindCSS")
+}
+
+tasks.named("test") {
+    dependsOn("generateTailwindCSS")
+}
+
+
+fun createProjectVersion(): String {
+    var projectVersion = "$versionMajor.$versionMinor-SNAPSHOT"
+
+    // get variables from github action workflow run (CI)
+    val githubRef = System.getenv("GITHUB_REF")
+    val githubRunNumber = System.getenv("GITHUB_RUN_NUMBER")
+    if (githubRef == "refs/heads/main" && githubRunNumber != null) {
+        // if run on CI set the version to the github run number
+        projectVersion = "$versionMajor.$versionMinor.$githubRunNumber"
+    }
+    return projectVersion
 }
