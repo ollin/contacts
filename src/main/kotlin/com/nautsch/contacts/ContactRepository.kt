c58c7f4418c4536289cef247e7bf9584abc3e978
Oliver Nautsch <oliver@brixel.io>
2024-04-08 10:12:16 +0200

    wip: create data

--- src/main/kotlin/com/nautsch/contacts/ContactRepository.kt
+++ src/main/kotlin/com/nautsch/contacts/ContactRepository.kt
@@ -1,4 +1,35 @@
 package com.nautsch.contacts
 
+import io.github.serpro69.kfaker.Faker
+import java.util.*
+
 class ContactRepository {
+
+    private var idContactMap: Map<UUID, Contact>
+
+    init {
+        val faker = Faker()
+        val tmpIdContactMap = mutableMapOf<UUID, Contact>()
+        (1..100).forEach {
+            val tmpFirstName = faker.name.firstName()
+            val tmpLastName = faker.name.lastName()
+            val tmpId = UUID.randomUUID()
+            tmpIdContactMap.put(
+                tmpId,
+                Contact(
+                    id = tmpId,
+                    firstName = tmpFirstName,
+                    lastName = tmpLastName,
+                    email = faker.internet.safeEmail("${tmpFirstName}.${tmpLastName}".replace(" ", "")),
+                    phone = faker.phoneNumber.phoneNumber()
+                )
+            )
+        }
+
+        idContactMap = tmpIdContactMap
+    }
+
+    fun listAll(): List<Contact> {
+        return idContactMap.values.toList()
+    }
 }
